735d0e202ad90d8f1ff5ab1515e8d8ef761a306a
dsk502 <shengkang2023@outlook.com>
2025-04-05 23:39:06 +0800

    Fixed reference

--- src/home_irrigation_server.cpp
+++ src/home_irrigation_server.cpp
@@ -95,7 +95,7 @@ int HomeIrrigationServer::server_init() {
     } else {    //If the device is not added
         //Do nothing
     }
-    m_net_thread_obj = new NetworkingThread(m_server_info_database_helper, m_watering_record_helper, &m_is_added, m_server_id, m_server_info, &m_pump_thread_obj, &m_soil_moisture_thread_obj);
+    m_net_thread_obj = new NetworkingThread(m_server_info_database_helper, m_watering_record_helper, &m_is_added, m_server_id, m_server_info, m_pump_thread_obj, m_soil_moisture_thread_obj);
     //m_net_thread = new std::thread(m_net_thread_obj->networking_thread_main);
     //m_net_thread.detach();
 
