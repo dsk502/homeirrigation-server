2a7a9269708bda46d0aa8e0e9016978e7a3d6ab5
dsk502 <shengkang2023@outlook.com>
2025-04-06 11:07:53 +0800

    Fix bugs

--- include/main/thread_objects.hpp
+++ include/main/thread_objects.hpp
@@ -1,10 +1,6 @@
 #ifndef THREADOBJECTS_HPP
 #define THREADOBJECTS_HPP
 
-#include "hardware_control/pump_thread.hpp"
-#include "hardware_control/soil_moisture_thread.hpp"
-#include "networking/networking_thread.hpp"
-
 typedef struct thread_objects {
     PumpThread* pump_thread_obj;
     SoilMoistureThread* soil_moisture_thread_obj;
